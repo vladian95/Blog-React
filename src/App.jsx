f0403f530629d112122af3d04134091004fe13a9
vladian95 <kvankvan1995@gmail.com>
2022-10-25 20:18:16 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TopBar from '../src/topbar/TopBar';
+
+function App() {
+  return (
+    <>
+      <TopBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,11 @@
+import './topbar.css';
+
+export default function TopBar() {
+  return (
+    <div className="Top">
+      <div className="TopLeft">l</div>
+      <div className="TopCenter">c</div>
+      <div className="TopRight">r</div>
+    </div>
+  );
+}
