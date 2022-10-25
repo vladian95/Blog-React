7c67b4a8a3981c5fd4b0adfb3f8b32a16bb455c4
vladian95 <kvankvan1995@gmail.com>
2022-10-25 20:53:42 +0200

    topbar

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,9 +3,29 @@ import './topbar.css';
 export default function TopBar() {
   return (
     <div className="Top">
-      <div className="TopLeft">l</div>
-      <div className="TopCenter">c</div>
-      <div className="TopRight">r</div>
+      <div className="TopLeft">
+        <i className="TopIcon fa-brands fa-square-facebook"></i>
+        <i className="TopIcon fa-brands fa-square-twitter"></i>
+        <i className="TopIcon fa-brands fa-square-pinterest"></i>
+        <i className="TopIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="TopCenter">
+        <ul className="TopList">
+          <li className="TopListItem">HOME</li>
+          <li className="TopListItem">ABOUT</li>
+          <li className="TopListItem">CONTACT</li>
+          <li className="TopListItem">WRITE</li>
+          <li className="TopListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="TopRight">
+        <img
+          className="TopImg"
+          src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          alt=""
+        />
+        <i className="TopSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 }
