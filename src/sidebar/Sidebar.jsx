65806db6dc03abfad2593f909516326dd981b1df
vladian95 <kvankvan1995@gmail.com>
2022-11-01 09:26:11 +0100

    start sibebar

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -6,13 +6,24 @@ export default function Sidebar() {
       <div className="SidebarItem">
         <span className="SidebarTitle">About Me</span>
         <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          src="https://st2.depositphotos.com/3272717/8196/i/450/depositphotos_81965018-stock-photo-blond-girl-in-a-field.jpg"
           alt=""
         />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, beatae,
           quae, provident ipsum eum dolorem quibusdam
         </p>
+      </div>
+      <div className="SidebarItem">
+        <span className="SidebarTitle">CATEGORIES</span>
+        <ul className="SidebarLIst">
+          <li className="SidebarListItem">Life</li>
+          <li className="SidebarListItem">Music</li>
+          <li className="SidebarListItem">Style</li>
+          <li className="SidebarListItem">Sport</li>
+          <li className="SidebarListItem">Tech</li>
+          <li className="SidebarListItem">Cinema</li>
+        </ul>
       </div>
     </div>
   );
