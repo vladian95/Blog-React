458fd25946f1d237db787521986c136aa14f0301
vladian95 <kvankvan1995@gmail.com>
2022-11-04 20:12:49 +0100

    react eouter dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="single" element={<Single />} />
+        <Route path="write" element={<Write />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
